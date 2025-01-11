9940f1b94651377881cc45befa3834530d2515ef
Janeth <zanetevelyn@gmail.com>
2025-01-11 04:59:26 -0800

    Delete Besar samadengan.py

--- Besar samadengan.py
+++
@@ -1,7 +0,0 @@
-umur = int(input("Masukkan umur anda: "))
-
-
-if umur >= 17:
-    print("Anda sudah cukup umur untuk mendapatkan SIM.")
-else :
-    print("Anda belum cukup umur untuk mendapatkan SIM.")
